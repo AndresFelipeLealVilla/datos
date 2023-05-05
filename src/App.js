1e381fc5089daad912385a8eab034b2d2e26b213
Andrés Felipe Leal Villa <38890922+AndresFelipeLealVilla@users.noreply.github.com>
2023-05-04 21:43:20 -0500

    actualización home

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/home" element={<Home/>} />
+        <Route path="/datos" element={<Home/>} />
         <Route path="/about" element={<About/>} />
         <Route path="/portafolio" element={<Portafolio/>} />
         <Route path="/blog" element={<Blog/>} />
